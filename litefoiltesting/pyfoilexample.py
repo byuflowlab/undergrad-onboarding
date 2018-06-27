e64f9f21bd0adb1d377abc792fa5045ac55d58a5
Judd Mehr <juddmehr@gmail.com>
2018-06-27 15:48:43 -0600

    make some fixes

--- litefoiltesting/pyfoilexample.py
+++ litefoiltesting/pyfoilexample.py
@@ -6,19 +6,19 @@ from naca import naca4 as naca
 from af import xspacing
 
 # --- Set Parameters --- #
-Re = 1e6
+Re = 1e7
 Mach = 0.0
-maxiter = 10
+maxiter = 100
 alpha = 2.0 # np.array(np.linspace(-3,7,20))
-p = [0.015, 8.0, 7.5, 1.0, 1.0] #[2,4,12]
+p = [4.49527659,  3.32387216, 11.23074539,  3.38520706] #[0.015, 8.0, 7.5, 1.0, 1.0] #[2,4,12]
 
 # --- Create Airfoil Geometry --- #
 xaf = xspacing()
-x,z = bsp(p,xaf) #naca(p,xaf)
-print x
+x,z = naca(p,xaf) #bsp(p,xaf) #
+# print x
 # --- Run Xfoil --- #
 cl, cd, cm, conv = pyfoil.runxfoil(x,z,alpha,Re,Mach,maxiter)
-
+print cl, cd, conv
 # --- Plot Stuff ---#
 
 #single af fig:
