fab3e55183704be6d351127d2fdf29cf388cfc5b
Adam Cardoza <adam@cardoza.one>
2018-07-01 00:00:50 -0600

    Continuing work on jfoiltesting.jl, currently saving all data points as different arrays because I'm working on how to use multi-dimensional arrays and collections.

---
+++ litefoiltesting/exploring_julia.jl
@@ -0,0 +1,10 @@
+workspace()
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = zeros(1,3)
+
+
+
+blue = Array{Float64}(3,3)
+
+print("done")
--- litefoiltesting/jfoiltesting.jl
+++ litefoiltesting/jfoiltesting.jl
@@ -1,6 +1,6 @@
 import Xfoil
 import AirfoilParams
-import PyPlot
+#using PyPlot
 
 
 """
@@ -10,43 +10,78 @@ determine if there is a difference in data, and if so how much.
 Author: Adam Cardoza
 Date started: 6/28/18
 Inputs: A range of aoa, Re, and possibly Mach numbers or NACA parameters.
-Outputs: A txt or csv file that I can use to analyze data.
+Outputs: Currently, print some data, but possibly have either a txt file,
+figures or plots that help present the data.
 Notes: While reading about Pycall, we might be getting some overhead from using
 Pycall itself.
 """
 
-
-
 ## Setting Parameters ##
-Re_exponent = [5, 6]
-Mach = 0.0
+Re_exponent = [4, 8]
+Mach_array= [0.0, 0.8]
 maxiter = 10
-alpha = [2.0, 3.0]
-p = [2,4,12]
+aoa = collect(linspace(-5,20,10))  #aoa must be a vector for xfoilsweep to work
+p1 = [0, 6]
+p2 = [2, 4]
+p3 = [5, 16]
+
+#Initializing Variables and Arrays
+count = 0
+P1 = []
+P2 = []
+P3 = []
+MACH = []
+RE = []
+CL = []
+CD = []
+CDP = []
+CM = []
+CONVERGED = []
 
 println("")
 println("Begin New Run")
 println("")
 
-## --- Create Airfoil Geometry --- ##
-x,z = AirfoilParams.naca4(p[1],p[2],p[3])
+## Iterate through the different combination of P values
+for i in 1:length(p1)
+    for j in 1:length(p2)
+        for k in 1:length(p3)
+            p = [p1[i] p2[j] p3[k]]
+            ## Create Airfoil Geometry ##
+            x,zu,zl = AirfoilParams.naca4(p[1],p[2],p[3])
+            #concantenate the upper and lower, and make the vectors the same length
+            xx = vcat(x[end:-1:1], x)
+            zz = vcat(zu[end:-1:1],zl)
 
-## Prepare for single run, cause I don't know how to use the sweep function yet
-Xfoil.setCoordinates(x,z)
-
-## Prepare txt file for output ##
-outfile = "jfoil1.txt"
-f = open(outfile,"w")
-#println(f, "Re", "  ", "aoa", "  ", " cl", "  ", "cd", "  ")
-
-#Iterate through the different combination of values
-for i in 1:length(Re_exponent)
-    for j in 1:length(alpha)
-        aoa = alpha[j]
-        Re = float(1*10^Re_exponent[i])
-        cl, cd, cdp, cm, converged = Xfoil.solveAlpha(aoa,Re,mach=Mach,iter=maxiter)
-        println(f, "1e", Re_exponent[i], "  ", aoa, "  ", cl, "  ", cd, "  ", )
+            # iterate through the different Re and Mach values
+            for l in 1:length(Mach_array)
+                for m in 1:length(Re_exponent)
+                    Mach = Mach_array[l]
+                    count += 1
+                    Re = float(1 * 10^Re_exponent[m])
+                    cl, cd, cdp, cm, converged = Xfoil.xfoilsweep(xx,zz,aoa,Re,mach=0.0,iter=10)
+                    append!(P1,p[1])
+                    append!(P2,p[2])
+                    append!(P3,p[3])
+                    append!(MACH,Mach)
+                    append!(RE,Re)
+                    print("[", P1[count], ",", P2[count], ",", P3[count], "]", " ")
+                    println(RE[count], "  ", MACH[count], "  ", aoa, "  ", cl, "  ", cd, "  ", )
+                    println(" ")
+                end
+            end
+        end
     end
 end
 
-close(f)
+
+#for i in 1:length(Re_exponent)
+    #for j in 1:length(alpha)
+        #Re = float(1*10^Re_exponent[i])
+        #cl, cd, cdp, cm, converged = Xfoil.xfoilsweep(xx,zz,aoa,Re,mach=0.0,iter=100)
+        #println("1e", Re_exponent[i], "  ", aoa, "  ", cl, "  ", cd, "  ", )
+    #end
+#end
+
+#println(" ")
+#println("PyXlight data")
