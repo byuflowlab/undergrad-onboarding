2960256606a14fef2e52b9ae3f1cbc879d191ac0
Adam Cardoza <adam@cardoza.one>
2018-07-05 16:01:46 -0600

    Finished comparing input data, changing error analysis to focus around convergence overlap

--- litefoiltesting/jfoiltesting.jl
+++ litefoiltesting/jfoiltesting.jl
@@ -19,33 +19,50 @@ Outputs: Currently, print some data, but possibly have either a txt file,
 figures or plots that help present the data.
 
 Notes: I am accomplishing this by running all of the data using xfoil.jl, after
-I will collect my PyXlight data by using pycall.
+I will collect my PyXlight data by using pycall. USES FILE pyfoil.py that Judd created
+for me. Not a normal part of pyXlight, may consider using other file.
 """
 
 
-## Setting Parameters ##
-Re_exponent = [4, 8]
+## Setting Parameters for data collection ##
+Re_exponent = [4, 8]    #This is the exponent of the reynolds number. It creates an appropriate array below.
 Mach_array= [0.0, 0.1] #Got a warning/error sourced from Xfoil saying
 #CPCALC Local speed too fast, (Or something like that) when Mach == 0.8
-iterations = 50
-per_maint = true    #Percussive Maintenance.
-aoa_length = 10
+iterations = 20     #This is the number of iterations that Xfoil goes through to calc
+per_maint = false    #Percussive Maintenance.
+aoa_length = 10     #determines the number of entries in aoa array
+x_length= 100       #Julia is automatically set at 100, and can't change without
+#changing source code. This is half the number of points entered into Xfoil for the
+#shape of the wing.
 aoa = collect(linspace(-5,20,aoa_length))  #aoa must be a vector for xfoilsweep to work
 X = reshape(aoa,1,aoa_length)
 p1 = [0, 6]
 p2 = [2, 4]
 p3 = [5, 16]
 
+## Setting parameters for data analysis ##
+error_tol = 5 #written in percent. Will notify if regions have error greater than error_tol
+show_max = 1    #for all show vars, if 1, will print said data to screen
+show_min = 1
+show_avg = 1
+show_time = 1
+
 #Initializing Variables and Arrays
 count = 0
 num_calcs = length(p1)*length(p2)*length(p3)*length(Mach_array)*length(Re_exponent)
-P = [0 0 0]
-MACH = []
-RE = []
+Pj = [0 0 0]
+Ppy = [0 0 0]
+MACHj = []
+MACHpy= []
+REj = []
+REpy = []
 
 # Due to array adding rules, I initialized storage arrays to zero, and will
 # reset the initial value later with an if statement.
-
+Xj = zeros(Float64, 1, 2*x_length)
+Xpy = zeros(Float64, 1, 2*x_length)
+Zj = zeros(Float64, 1, 2*x_length)
+Zpy = zeros(Float64, 1, 2*x_length)
 CLj = zeros(X)
 CDj = zeros(X)
 CDPj = zeros(X)
@@ -70,7 +87,8 @@ for i in 1:length(p1)
             p = [p1[i] p2[j] p3[k]]
 
             ## Create Airfoil Geometry ##
-            x,zu,zl = AirfoilParams.naca4(p[1],p[2],p[3])
+            x,zu,zl = AirfoilParams.naca4(p[1],p[2],p[3],x_length,bluntTE=true) #c, p, t, N ( number of iterations), bluntTE
+                #the python naca file doesn't have a bluntTE file, could be calling incorrectly
 
             #concantenate the upper and lower, and make the vectors the same length
             xx = vcat(x[end:-1:1], x)
@@ -86,32 +104,45 @@ for i in 1:length(p1)
 
                     #Run xfoil
                     cl, cd, cdp, cm, converged = Xfoil.xfoilsweep(xx,zz,aoa,Re,mach=Mach,iter=iterations,npan=140,percussive_maintenance=per_maint)
+                    # xx & zz are the shape of the airfoil, created earlier -- differences shown below
+                    #aoa is the array of angle of attacks -- the code puts the exact same array in both calls
+                    #Re is the current Reynolds number -- both calls receive same vars
+                    #mach is the current mach number (airspeed?) -- same as Re
+                    #iter is the number of iterations the function attemps to solve? -- same on both calls
+                    #npan is the the number of panels used in Xfoil, julia default=140, python default=??? xfoil default=140.
+                    #percussive_maintenance is a bool of whether or not percussive_maintenance is used -- same on both calls
 
                     #Format and save Data
+                    xtemp = reshape(xx, 1, 2*x_length)
+                    ztemp = reshape(zz, 1, 2*x_length)
                     cl = reshape(cl,1,length(cl))
                     cd = reshape(cd,1,length(cd))
                     cdp = reshape(cdp,1,length(cdp))
                     cm = reshape(cm,1,length(cm))
                     converged = reshape(converged,1,length(converged))
                     if count == 1   #reseting the initial values to actual
-                        P = P + p
-                        CLj = CLj + cl
-                        CDj = CDj + cd
-                        CDPj = CDPj + cdp
-                        CMj = CMj + cm
-                        CONVERGEDj = CONVERGEDj + converged
+                        Xj += xtemp
+                        Zj += ztemp
+                        Pj += p
+                        CLj += cl
+                        CDj += cd
+                        CDPj += cdp
+                        CMj += cm
+                        CONVERGEDj += converged
                     else
-                        P = vcat(P,p)
+                        Xj = vcat(Xj,xtemp)
+                        Zj = vcat(Zj,ztemp)
+                        Pj = vcat(Pj,p)
                         CLj = vcat(CLj,cl)
                         CDj = vcat(CDj,cd)
                         CDPj = vcat(CDPj,cdp)
                         CMj = vcat(CMj,cm)
                         CONVERGEDj = vcat(CONVERGEDj, converged)
                     end
-                    append!(MACH,Mach)
-                    append!(RE,Re)
-                    #println("[", P[count,1], ", ", P[count,2], ", ",P[count,3],"]" )
-                    #println(RE[count], "  ", MACH[count], "  ", aoa, "  ", cl, "  ", cd, "  ", )
+                    append!(MACHj,Mach)
+                    append!(REj,Re)
+                    #println("[", Pj[count,1], ", ", Pj[count,2], ", ",Pj[count,3],"]" )
+                    #println(REj[count], "  ", MACHj[count], "  ", aoa, "  ", cl, "  ", cd, "  ", )
                     #println(converged)
                     percent_done = 100*count/num_calcs
                     println("Julia Calculation Percent Done: ", percent_done,"%")
@@ -136,7 +167,8 @@ for i in 1:length(p1)
             p = [p1[i], p2[j], p3[k]]
 
             ## Create Airfoil Geometry ##
-            xaf = af.xspacing(80) #80 is the number that is set in AirfoilParams.jl
+            xaf = af.xspacing(x_length) #80 is the number that is set in AirfoilParams.jl
+            #line 3, however in naca.jl line 41 it is called with 100
             x,z = NACA.naca4(p,xaf)
 
             # iterate through the different Re and Mach values
@@ -150,21 +182,32 @@ for i in 1:length(p1)
                     #Run xfoil
                     cl, cd, cm, converged = pyfoil.runxfoil(x, z, aoa, Re, Mach, iterations, per_maint)
                     #Format and save Data
+                    xtemp = reshape(x, 1, 2*x_length)
+                    ztemp = reshape(z, 1, 2*x_length)
+                    ptemp = reshape(p,1,3)
                     cl = reshape(cl,1,length(cl))
                     cd = reshape(cd,1,length(cd))
                     cm = reshape(cm,1,length(cm))
                     converged = reshape(converged,1,length(converged))
                     if count == 1   #reseting the initial values to actual
-                        CLpy = CLpy + cl
-                        CDpy = CDpy + cd
-                        CMpy = CMpy + cm
+                        Xpy += xtemp
+                        Zpy += ztemp
+                        Ppy += ptemp
+                        CLpy += cl
+                        CDpy += cd
+                        CMpy += cm
                         CONVERGEDpy = CONVERGEDpy + converged
                     else
+                        Xpy = vcat(Xpy, xtemp)
+                        Zpy = vcat(Zpy, ztemp)
+                        Ppy = vcat(Ppy, ptemp)
                         CLpy = vcat(CLpy,cl)
                         CDpy = vcat(CDpy,cd)
                         CMpy = vcat(CMpy,cm)
                         CONVERGEDpy = vcat(CONVERGEDpy, converged)
                     end
+                    append!(MACHpy, Mach)
+                    append!(REpy, Re)
                     #println("[", P[count,1], ", ", P[count,2], ", ",P[count,3],"]" )
                     #println( aoa, "  ", cl, "  ", cd, "  ", )
                     #println(converged)
@@ -185,54 +228,98 @@ println("Start Analysis")
 println("###############################")
 println(" ")
 
-#calulate error
-temp = CLj-CLpy
-clerror = 100*temp./CLpy
-temp = CDj - CDpy
-cderror = 100*temp./CDpy
-temp = CMj - CMpy
-cmerror = 100*temp./CMpy
+#printout % converged
+#Something to indicate where they overlap in convergence
+#Collect the range of data that show where they converge and where
+#   they don't. Make note of any trends.
+#Only show error if they both converge
+#Min error doesn't matter, max and avg are good
+#
+
+#Determine Convergence overlap and difference
+conv_comp = (2*CONVERGEDpy)-CONVERGEDj
+conv_both = find(conv_comp .== 1)
+conv_j = find(conv_comp .== -1)
+conv_py = find(conv_comp .== 2)
+conv_none = find(conv_comp .== 0)
+
+"""
+#calulate total error
+xerror = 100*(Xpy - Xj)./Xpy
+zerror = 100*(Zpy - Zj)./Zpy
+clerror = 100*(CLpy-CLj)./CLpy
+cderror = 100*(CDpy-CDj)./CDpy
+cmerror = 100*(CMpy-CMj)./CMpy
 
 #Error extrema
-clerror_max = maximum(clerror)
-clerror_min = minimum(clerror)
-cderror_max = maximum(cderror)
-cderror_min = minimum(cderror)
-cmerror_max = maximum(cmerror)
-cmerror_min = minimum(cmerror)
+xerror_max = findmax(xerror)
+xerror_min = findmin(xerror)
+zerror_max = findmax(zerror)
+zerror_min = findmin(zerror)
+clerror_max = findmax(clerror)
+clerror_min = findmin(clerror)
+cderror_max = findmax(cderror)
+cderror_min = findmin(cderror)
+cmerror_max = findmax(cmerror)
+cmerror_min = findmin(cmerror)
 
 #Calculate average error
+xavg_error = 0.0
+zavg_error = 0.0
 clavg_error = 0.0
 cdavg_error = 0.0
 cmavg_error = 0.0
 for i in 1:length(clerror)
+    xavg_error += xerror[i]
+    zavg_error += zerror[i]
     clavg_error += clerror[i]
     cdavg_error += cderror[i]
     cmavg_error += cmerror[i]
 end
+xavg_error = xavg_error/length(xerror)
+zavg_error = zavg_error/length(zerror)
 clavg_error = clavg_error/length(clerror)
 cdavg_error = cdavg_error/length(cderror)
 cmavg_error = cmavg_error/length(cmerror)
+"""
 
 #time analysis
 time_ratio = julia_time/python_time
 time_diff = julia_time - python_time
 
 #Print outcomes
-println("Max error: ")
-println("   ", "cl error: ", clerror_max)
-println("   ", "cd error: ", cderror_max)
-println("   ", "cm error: ", cmerror_max)
-println(" ")
-println("Min error: ")
-println("   ", "cl error: ", clerror_min)
-println("   ", "cd error: ", cderror_min)
-println("   ", "cm error: ", cmerror_min)
-println(" ")
-println("Avg error: ")
-println("   ", "cl error: ", clavg_error)
-println("   ", "cd error: ", cdavg_error)
-println("   ", "cm error: ", cmavg_error)
-println(" ")
-println("Time ratio: ", time_ratio, " (Julia/Python)")
-println("Time diff: ", time_diff, " (Julia-Python)")
+if show_max == 1
+    println("Max error: ")
+    #if xerror_max[1] >= error_tol
+    println("   ", "x error: ", xerror_max, "%")
+    #end
+    #if zerror_max[1] >= error_tol
+    println("   ", "z error: ", zerror_max, "%")
+    #end
+    println("   ", "cl error: ", clerror_max, "%")
+    println("   ", "cd error: ", cderror_max, "%")
+    println("   ", "cm error: ", cmerror_max, "%")
+    println(" ")
+end
+if show_min == 1
+    println("Min error: ")
+    println("   ", "x error: ", xerror_min, "%")
+    println("   ", "z error: ", zerror_min, "%")
+    println("   ", "cl error: ", clerror_min, "%")
+    println("   ", "cd error: ", cderror_min, "%")
+    println("   ", "cm error: ", cmerror_min, "%")
+    println(" ")
+end
+if show_avg == 1
+    println("Avg error: ")
+    println("   ", "x error: ", xavg_error, "%")
+    println("   ", "z error: ", zavg_error, "%")
+    println("   ", "cl error: ", clavg_error, "%")
+    println("   ", "cd error: ", cdavg_error, "%")
+    println("   ", "cm error: ", cmavg_error, "%")
+    println(" ")
+end
+if show_time == 1
+    println("Time ratio: ", time_ratio, " (Julia/Python)")
+    println("Time diff: ", time_diff, " (Julia-Python)")
+end
